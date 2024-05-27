f498ca890a958c4f8a6f7f29e010aff70908c293
ceejay-dev <candidojoao12@gmail.com>
2024-05-27 17:13:45 +0100

    update in homeage - front

--- karapinha-web/src/views/Home.tsx
+++ karapinha-web/src/views/Home.tsx
@@ -13,64 +13,55 @@ export function Home() {
 
   return (
     <main>
-      <header>
-          <nav>
-            <ul>
-              <li>
-              <Link to='/'>Home</Link>
-              </li>
-              <li>
-              <Link to='/login'>Serviços</Link>
-              </li>
-              <li>
-              <Link to='/'>Sobre</Link>
-              </li>
-
-            </ul>
-          </nav>
-        </header>
-
       <div className="carousel-container">
-          <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-              <img
-                className="img"
-                src= {carrosel1}
-                alt="Primeiro slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="carousel-container img"
-                src= {carrosel2}
-                alt="Segundo slide"
-              />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="carousel-container img"
-                src= {carrosel3}
-                alt="Terceiro slide"
-              />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+        <ul className="nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Serviços</Link>
+          </li>
+          <li>
+            <Link to="/">Sobre</Link>
+          </li>
+        </ul>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <img
+              className="carousel-container img"
+              src={carrosel1}
+              alt="Primeiro slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="carousel-container img"
+              src={carrosel2}
+              alt="Segundo slide"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="carousel-container img"
+              src={carrosel3}
+              alt="Terceiro slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     </main>
   );
